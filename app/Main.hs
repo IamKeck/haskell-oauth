cb75bfca9385bc1d39e9afc20c975d59d93f7c36
Kent Kawanaka <meal.is.tharihyu@gmail.com>
2018-03-26 03:43:16 +0900

    ショートカット関数を使ってメイン処理を書き直し

--- app/Main.hs
+++ app/Main.hs
@@ -12,16 +12,7 @@ main = do
   let url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
   let param = [("count", "5"), ("exclude_replies", "false")]
   key <- TwitterKey <$> (getEnv "CK") <*> (getEnv "CS") <*> (getEnv "AT") <*> (getEnv "AS")
-  auth_base <- createAuthBaseDefault key
-  let hmac_key = createHmacKey key 
-  let signature = createSignature False url param auth_base hmac_key
-  let auth_header = createAuthHeader auth_base signature
-  let bs_param = map (\(f,s) -> (CB.pack f, Just . CB.pack $ s)) param
-  request <- parseRequest url >>=
-    return . (addRequestHeader "Authorization" $ CB.pack auth_header) .
-    setRequestQueryString bs_param
-  response <- httpBS request
-  print $ getResponseBody response
+  createRequest False url param [] key >>= httpBS >>= print . getResponseBody
 
 
 
